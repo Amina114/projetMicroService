6a33219ddc0925eef7b357f4ad49359b1b61f04e
achabchoub <achabchoub@vermeg.com>
2023-03-04 15:29:17 +0100

    Add entity

--- src/main/java/com/microservice/candidat/entity/Candidature.java
+++ src/main/java/com/microservice/candidat/entity/Candidature.java
@@ -3,18 +3,18 @@ package com.microservice.candidat.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Id;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public
-class Candidature {
+public class Candidature {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
