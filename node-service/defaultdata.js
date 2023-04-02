5a67bbf2123b5799f1c56be916ab5b5af2941be2
Fadhel-BH <fadhel.bhamida@gmail.com>
2023-04-02 20:00:41 +0100

    commits

---
+++ node-service/defaultdata.js
@@ -0,0 +1,53 @@
+module.exports = {
+  name: 'Fadhel Ben Hamida',
+  email: 'fadhel.bhamida@gmail.com',
+  phone: '+216 52636541',
+  skills: [
+    'Postgresql', 'Rest',
+    'English',    'Docker',       
+    'C',          'Jira',
+    'Html',       'Ios',
+    'French',     'Engineering',
+    'Android',    'Programming',
+    'Aws',        'Algorithms',
+    'Github',     'Javascript',
+    'Php',        'Python',
+    'Mysql',      'Scrum',
+    'Css',        'Api',
+    'C++',        'Computer science',
+    'Front-end'
+  ],
+  education: [
+    {
+      name: 'Higher Institute Of Management ISG Tunisia',
+      dates: [Array],
+      date_start: 'January 2017',
+      date_end: 'June 2020'
+    },
+    {
+      name: 'Higher Institute Of Computer Science Tunisia',
+      dates: [Array],
+      date_start: 'January 2017',
+      date_end: 'June 2020'
+    },
+    {
+      name: 'Private High School of Engineering and Technologies',
+      dates: [Array],
+      date_start: 'January 2017',
+      date_end: 'June 2020'
+    },
+    {
+      name: 'Computer Science Private High School of Engineering and Technologies',
+      dates: [Array],
+      date_start: 'January 2017',
+      date_end: 'June 2020'
+    }
+  ],
+  experience: [
+    {
+      title: 'Software Developer',
+      dates: [Array],
+      organization: 'VERMEG FACTORY TUNISIA'
+    }
+  ]
+}
