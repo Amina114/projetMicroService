b18e6ba72c58cf747544d42c540d60cf7569ac7f
Fadhel-BH <fadhel.bhamida@gmail.com>
2023-04-07 20:31:21 +0100

    doc word

--- employee-service/src/main/java/com/example/employeeservice/config/SwaggerConfiguration.java
+++ employee-service/src/main/java/com/example/employeeservice/config/SwaggerConfiguration.java
@@ -34,7 +34,7 @@ public class SwaggerConfiguration {
                 "App to demonstrate MicroService based on Spring Boot",
                 "0.0.1-SNAPSHOT",
                 "Terms of Service",
-                new Contact("Adib Ouechani",
+                new Contact("Fadhel Ben Hmida",
                         "",
                         ""),
                 "",
